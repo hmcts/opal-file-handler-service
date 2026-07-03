ea79ea1628868034140938048e131b1fba51a7ba
Sam Naylor <sam.naylor@hmcts.net>
2026-07-03 16:21:12 +0100

    PO-5617 - add task ITs

---
+++ src/integrationTest/java/uk/gov/hmcts/opal/filehandler/config/task/AutomatedTaskCapsReportIntegrationTest.java
@@ -0,0 +1,42 @@
+package uk.gov.hmcts.opal.filehandler.config.task;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import java.io.IOException;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockReset;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.web.servlet.DispatcherServlet;
+import uk.gov.hmcts.opal.filehandler.support.AbstractIntegrationTest;
+
+@ActiveProfiles("integration")
+@SpringBootTest(properties = {
+    "opal.automated-task=CAPSReport",
+    "spring.main.web-application-type=none"
+})
+public class AutomatedTaskCapsReportIntegrationTest extends AbstractIntegrationTest {
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @MockitoBean(enforceOverride = true, reset = MockReset.NONE)
+    private AutomatedCapsReport automatedCapsReport;
+
+    @Test
+    void shouldNotCreateWebLayer() {
+        assertThat(applicationContext.containsBean("dispatcherServlet")).isFalse();
+        assertThat(applicationContext.getBeansOfType(DispatcherServlet.class).isEmpty()).isTrue();
+    }
+
+    @Test
+    void shouldCallAutomatedTaskRun() throws IOException {
+        verify(automatedCapsReport, times(1)).run(any());
+    }
+}
