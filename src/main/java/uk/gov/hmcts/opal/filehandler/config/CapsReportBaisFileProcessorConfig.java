c3187d1d2f8bc9c86e0a2a2ba6eafa4c3ce17f5b
Thomas Croasdale <288008923+thomas-croasdale-cgi@users.noreply.github.com>
2026-07-21 15:00:09 +0100

    PO-3948 Fix style

--- src/main/java/uk/gov/hmcts/opal/filehandler/config/CapsReportBaisFileProcessorConfig.java
+++ src/main/java/uk/gov/hmcts/opal/filehandler/config/CapsReportBaisFileProcessorConfig.java
@@ -1,8 +1,6 @@
 package uk.gov.hmcts.opal.filehandler.config;
 
-import lombok.Getter;
 import lombok.Setter;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
