41bf097d77ec5883fbf846426f89573e0c6d4a12
Thomas Croasdale <288008923+thomas-croasdale-cgi@users.noreply.github.com>
2026-07-22 10:04:04 +0100

    PO-3948 Remove suppression

--- src/main/java/uk/gov/hmcts/opal/filehandler/config/AbstractBaisFileProcessorConfig.java
+++ src/main/java/uk/gov/hmcts/opal/filehandler/config/AbstractBaisFileProcessorConfig.java
@@ -2,7 +2,6 @@ package uk.gov.hmcts.opal.filehandler.config;
 
 import lombok.Getter;
 import lombok.Setter;
-import lombok.Setter;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
