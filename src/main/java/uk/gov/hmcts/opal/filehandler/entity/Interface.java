d7bba96fab291a3bdf10f00927bcab5253f267fa
benedwards <ben.edwards@cgi.com>
2026-07-23 14:28:49 +0100

    Added Code updates + unit tests

--- src/main/java/uk/gov/hmcts/opal/filehandler/entity/Interface.java
+++ src/main/java/uk/gov/hmcts/opal/filehandler/entity/Interface.java
@@ -1,6 +1,15 @@
 package uk.gov.hmcts.opal.filehandler.entity;
 
 public enum Interface {
+    NATWEST,
+    ALLPAY,
+    ALLPAY_DD,
+    BARCLAYCARD,
+    BTECKOH,
+    DWP,
+    CDER,
+    JACOBS,
+    MARSTON,
     BTECKOH_REPORT,
     CAPS_REPORT,
     OPAL
