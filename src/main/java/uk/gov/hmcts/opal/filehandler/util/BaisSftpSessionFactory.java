ed9ea91fe64989c12bd01b3102590964e31c4d9e
Sam Naylor <sam.naylor@hmcts.net>
2026-07-24 12:06:18 +0100

    update SFTP connection

--- src/main/java/uk/gov/hmcts/opal/filehandler/util/BaisSftpSessionFactory.java
+++ src/main/java/uk/gov/hmcts/opal/filehandler/util/BaisSftpSessionFactory.java
@@ -2,12 +2,14 @@ package uk.gov.hmcts.opal.filehandler.util;
 
 import java.nio.charset.StandardCharsets;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.integration.sftp.session.DefaultSftpSessionFactory;
 import org.springframework.integration.sftp.session.SftpRemoteFileTemplate;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.opal.filehandler.config.BaisSftpConfiguration;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class BaisSftpSessionFactory {
@@ -20,6 +22,7 @@ public class BaisSftpSessionFactory {
         sessionFactory.setHost(configuration.getHost());
         sessionFactory.setPort(configuration.getPort());
         sessionFactory.setUser(username);
+        sessionFactory.setAllowUnknownKeys(true);
         sessionFactory.setPrivateKey(new ByteArrayResource(
             configuration.getPrivateKey().getBytes(StandardCharsets.UTF_8), "BAIS SFTP private key"));
 
