896b399f45fd2394dd68e0b64b15d98c9257fdee
Sam Naylor <sam.naylor@hmcts.net>
2026-07-24 13:27:00 +0100

    fix failing build

--- src/main/java/uk/gov/hmcts/opal/filehandler/config/BlobStorageConfiguration.java
+++ src/main/java/uk/gov/hmcts/opal/filehandler/config/BlobStorageConfiguration.java
@@ -4,9 +4,9 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@ConfigurationProperties("opal.file-handling-service.file-store")
 @Getter
 @Setter
+@ConfigurationProperties("opal.file-handler-service.file-store")
 public class BlobStorageConfiguration {
 
     private String storageAccountName;
