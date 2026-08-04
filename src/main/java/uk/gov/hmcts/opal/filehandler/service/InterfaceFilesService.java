4dfde95b8b9bd620c4584223f29e638790f4c9cb
jordan-muscott-cgi <288038593+jordan-muscott-cgi@users.noreply.github.com>
2026-08-04 12:02:53 +0100

    PO-3947 get interface files endpoint (#70)
    * update common lib and switch lombok and mapstruct annotation processors to get round order dependancy bug
    
    * initial work on specs, service and mapper
    
    * wire up mapper and service
    
    * add permissions check to service, and add service unit test
    
    * removed old comment
    
    * move service test to correct directory. Work on controller. Search dto mapper
    
    * SearchInterfaceFilesDtoMapper Test, add EI2 values to Type enum
    
    * controller unit tests
    
    * first integration tests and fix a few bugs
    
    * extra integration test to filter by date
    
    * uncomment out permissions code
    
    * add integration test to check for 403]
    
    * add feature flag
    
    * recorganise annotation processors in build.gradle to deal with mapstruct/lombok compatibility issue
    
    * remove dependency on common for building search Specification
    
    * change to use correct feature flag
    
    * re-write integration tests to be less brittle, ie cope with extra test data being added by other tickets
    
    * explicitly add IDs to sql scripts
    
    * removes comments from sql files
    
    * add checks to integration tests to check number or results
    
    * fixed style violations
    
    * fixed test name
    
    * added release to application yaml
    
    * add missing user service url to application-integration.yaml and give test better name
    
    * fixed error caused by date type change
    
    * add extra values to InterfaceFile enum in contract to bring in line with changes brought in from main
    
    * fix unit tests after changes to domain and commented out auth code
    
    * commented out integration test due to authorisation being disabled
    
    * fixed from_date search bug
    
    * changed comment indentation to satisfy checkstyle
    
    * changed comment indentation to satisfy checkstyle
    
    * changed formatting
    
    * changed formatting
    
    * added missing semi colon
    
    * changed formatting
    
    * changed formatting
    
    ---------
    
    Co-authored-by: Calvin <Calvin.Chu@HMCTS.NET>

--- src/main/java/uk/gov/hmcts/opal/filehandler/service/InterfaceFilesService.java
+++ src/main/java/uk/gov/hmcts/opal/filehandler/service/InterfaceFilesService.java
@@ -1,8 +1,46 @@
 package uk.gov.hmcts.opal.filehandler.service;
 
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.core.TypedPropertyPath;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import uk.gov.hmcts.opal.common.user.authorisation.exception.PermissionNotAllowedException;
+import uk.gov.hmcts.opal.common.util.SecurityUtil;
+import uk.gov.hmcts.opal.filehandler.authorisation.FileHandlerPermission;
+import uk.gov.hmcts.opal.filehandler.entity.InterfaceFileEntity;
+import uk.gov.hmcts.opal.filehandler.mapper.InterfaceFileMapper;
+import uk.gov.hmcts.opal.filehandler.repository.InterfaceFilesRepository;
+import uk.gov.hmcts.opal.filehandler.repository.specs.InterfaceFileSpecsFactory;
+import uk.gov.hmcts.opal.filehandler.service.request.SearchInterfaceFilesDto;
+import uk.gov.hmcts.opal.generated.model.InterfaceFileObjectInterfaceFile;
 
+@RequiredArgsConstructor
 @Service
 public class InterfaceFilesService {
 
+    private final InterfaceFilesRepository repository;
+    private final InterfaceFileSpecsFactory specsFactory;
+    private final InterfaceFileMapper mapper;
+
+    @Transactional(readOnly = true)
+    public List<InterfaceFileObjectInterfaceFile> searchInterfaceFiles(SearchInterfaceFilesDto request) {
+        // Permissions to be dealt with by: https://tools.hmcts.net/jira/browse/PO-8686
+        // checkPermissions();
+
+        Specification<InterfaceFileEntity> specs = specsFactory.createSearchSpecs(request);
+        Sort sort = Sort.by(Direction.ASC, TypedPropertyPath.of(InterfaceFileEntity::getCreatedDatetime));
+        List<InterfaceFileEntity> interfacesFiles = repository.findAll(specs, sort);
+        return mapper.toInterfaceFileObjects(interfacesFiles);
+    }
+
+    private void checkPermissions() {
+        if (!SecurityUtil.getOpalJwtAuthenticationTokenForCurrentUser()
+            .hasPermission(FileHandlerPermission.ViewInterfacesFile)) {
+            throw new PermissionNotAllowedException(FileHandlerPermission.ViewInterfacesFile);
+        }
+    }
 }
