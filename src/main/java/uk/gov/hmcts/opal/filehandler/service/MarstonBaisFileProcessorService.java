ed8744a8a429f9aacefe2664cba0e1e0ef036a61
Conroy White <conroywhite@uk-hk6f6nr9jf.lan>
2026-09-03 19:03:50 +0100

    PO-6439 Marston File transfer job service impl

---
+++ src/main/java/uk/gov/hmcts/opal/filehandler/service/MarstonBaisFileProcessorService.java
@@ -0,0 +1,54 @@
+package uk.gov.hmcts.opal.filehandler.service;
+
+import java.time.Clock;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.support.TransactionTemplate;
+import tools.jackson.databind.ObjectMapper;
+import uk.gov.hmcts.opal.filehandler.config.MarstonBaisFileBaisFileProcessorConfig;
+import uk.gov.hmcts.opal.filehandler.repository.InterfaceFilesRepository;
+import uk.gov.hmcts.opal.filehandler.service.blobstore.InterfaceFileBlobStoreService;
+import uk.gov.hmcts.opal.filehandler.service.extraction.ExtractionService;
+import uk.gov.hmcts.opal.filehandler.service.extraction.model.InterfaceFileCommonDataExtract;
+import uk.gov.hmcts.opal.filehandler.service.queue.InterfaceFilePreprocessQueueService;
+import uk.gov.hmcts.opal.filehandler.util.BaisSftpClient;
+import uk.gov.hmcts.opal.filehandler.util.FeatureFlagUtil;
+
+@Service
+public class MarstonBaisFileProcessorService
+    extends AbstractBaisInterfaceFileProcessorWithExtractionService<InterfaceFileCommonDataExtract> {
+
+    private final MarstonBaisFileBaisFileProcessorConfig config;
+
+    public MarstonBaisFileProcessorService(
+        Clock clock,
+        FeatureFlagUtil featureFlagUtil,
+        BaisSftpClient baisSftpClient,
+        InterfaceFileBlobStoreService interfaceFileBlobStoreService,
+        InterfaceFilesRepository interfaceFilesRepository,
+        TransactionTemplate transactionTemplate,
+        ObjectMapper objectMapper,
+        ExtractionService<InterfaceFileCommonDataExtract> extractionService,
+        InterfaceFilePreprocessQueueService finesQueueService,
+        InterfaceFilePreprocessQueueService maintenanceQueueService,
+        MarstonBaisFileBaisFileProcessorConfig config
+    ) {
+        super(
+            clock,
+            featureFlagUtil,
+            baisSftpClient,
+            interfaceFileBlobStoreService,
+            interfaceFilesRepository,
+            transactionTemplate,
+            objectMapper,
+            extractionService,
+            finesQueueService,
+            maintenanceQueueService
+        );
+
+        this.config = config;
+    }
+
+    public void run() {
+        super.run(config);
+    }
+}
