0fa94e6f62db739d4a7bf25366fc08723df0e88c
Thomas Croasdale <288008923+thomas-croasdale-cgi@users.noreply.github.com>
2026-09-04 10:33:02 +0100

    PO-6454 Limit configuration to 1 instance when testing-suport-endpoints is enabled

--- src/main/java/uk/gov/hmcts/opal/filehandler/config/task/TaskRunner.java
+++ src/main/java/uk/gov/hmcts/opal/filehandler/config/task/TaskRunner.java
@@ -1,7 +1,8 @@
 package uk.gov.hmcts.opal.filehandler.config.task;
 
-import lombok.RequiredArgsConstructor;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -10,9 +11,17 @@ import org.springframework.stereotype.Component;
 @Component
 @ConditionalOnProperty(name = "opal.automated-task")
 @Slf4j
-@RequiredArgsConstructor
 public class TaskRunner implements ApplicationRunner {
 
+    public TaskRunner(List<TaskConfiguration> configs, @Value("${opal.automated-task}") String task) {
+        if (configs.size() == 1) {
+            configuration = configs.getFirst();
+            return;
+        }
+        configuration = configs.stream()
+            .filter(c -> c.getClass().toString().toLowerCase().contains(task.toLowerCase()))
+            .findFirst().orElseThrow();
+    }
 
     private final TaskConfiguration configuration;
 
