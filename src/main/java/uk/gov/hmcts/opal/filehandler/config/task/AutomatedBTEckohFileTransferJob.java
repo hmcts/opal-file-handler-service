7299a3d1851eb1790baff8cd4bf5f6d5af14604e
Sam Naylor <sam.naylor@hmcts.net>
2026-09-07 16:04:14 +0100

    Align BTEckoh job with task runner

--- src/main/java/uk/gov/hmcts/opal/filehandler/config/task/AutomatedBTEckohFileTransferJob.java
+++ src/main/java/uk/gov/hmcts/opal/filehandler/config/task/AutomatedBTEckohFileTransferJob.java
@@ -1,26 +1,25 @@
 package uk.gov.hmcts.opal.filehandler.config.task;
 
-import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.opal.filehandler.config.BTEckohBaisFileProcessorConfiguration;
 import uk.gov.hmcts.opal.filehandler.service.BTEckohBaisFileProcessorService;
 
 @Component
 @RequiredArgsConstructor
-@ConditionalOnProperty(name = "opal.automated-task", havingValue = "BTEckohFileTransferJob")
+@ConditionalOnExpression(
+    "'${opal.automated-task}'.equals('BTEckohFileTransferJob') or ${opal.testing-support-endpoints.enabled}"
+)
 @Slf4j
-public class AutomatedBTEckohFileTransferJob implements ApplicationRunner {
+public class AutomatedBTEckohFileTransferJob implements TaskConfiguration {
 
     private final BTEckohBaisFileProcessorService fileProcessorService;
     private final BTEckohBaisFileProcessorConfiguration configuration;
 
     @Override
-    public void run(ApplicationArguments args) throws IOException {
+    public void run() {
         log.info("Starting BTEckoh File Transfer Job");
 
         fileProcessorService.run(configuration);
