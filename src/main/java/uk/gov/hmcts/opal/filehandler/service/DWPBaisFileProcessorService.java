69a76a30498dff188dad14b54986dc38282e920b
Thomas Croasdale <288008923+thomas-croasdale-cgi@users.noreply.github.com>
2026-09-07 14:40:21 +0100

    PO-6436 fix style failures

--- src/main/java/uk/gov/hmcts/opal/filehandler/service/DWPBaisFileProcessorService.java
+++ src/main/java/uk/gov/hmcts/opal/filehandler/service/DWPBaisFileProcessorService.java
@@ -1,13 +1,10 @@
 package uk.gov.hmcts.opal.filehandler.service;
 
-import java.io.InputStream;
 import java.time.Clock;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.support.TransactionTemplate;
 import tools.jackson.databind.ObjectMapper;
-import uk.gov.hmcts.opal.filehandler.config.BaisFileProcessorConfiguration;
-import uk.gov.hmcts.opal.filehandler.entity.InterfaceFileEntity;
 import uk.gov.hmcts.opal.filehandler.repository.InterfaceFilesRepository;
 import uk.gov.hmcts.opal.filehandler.service.blobstore.InterfaceFileBlobStoreService;
 import uk.gov.hmcts.opal.filehandler.service.extraction.PacsTTPBaisExtractionService;
