9e8f7abc4afa6d2b16b060a4a0744a658091bb20
Thomas Croasdale <288008923+thomas-croasdale-cgi@users.noreply.github.com>
2026-09-07 15:10:24 +0100

    PO-6436 Update to work with testing support endpoints

--- src/main/java/uk/gov/hmcts/opal/filehandler/config/task/AutomatedDWPFileTransferJob.java
+++ src/main/java/uk/gov/hmcts/opal/filehandler/config/task/AutomatedDWPFileTransferJob.java
@@ -1,26 +1,25 @@
 package uk.gov.hmcts.opal.filehandler.config.task;
 
-import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.opal.filehandler.config.DWPBaisFileProcessorConfiguration;
 import uk.gov.hmcts.opal.filehandler.service.DWPBaisFileProcessorService;
 
 @Component
-@ConditionalOnProperty(name = "opal.automated-task", havingValue = "DWPFileTransferJob")
+@ConditionalOnExpression(
+    "'${opal.automated-task}'.equals('DWPFileTransferJob') or ${opal.testing-support-endpoints.enabled}"
+)
 @Slf4j
 @RequiredArgsConstructor
-public class AutomatedDWPFileTransferJob implements ApplicationRunner {
+public class AutomatedDWPFileTransferJob implements TaskConfiguration {
 
     private final DWPBaisFileProcessorService processorService;
     private final DWPBaisFileProcessorConfiguration processorConfiguration;
 
     @Override
-    public void run(ApplicationArguments args) throws IOException {
+    public void run() {
         log.info("Starting automated DWP file transfer job");
 
         processorService.run(processorConfiguration);
