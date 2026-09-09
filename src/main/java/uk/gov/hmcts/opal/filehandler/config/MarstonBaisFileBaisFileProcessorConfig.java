7ae5f618db2d7fdb070117f4b7170569825c52e3
Conroy White <Conroy.White@HMCTS.NET>
2026-09-09 14:54:04 +0100

    make changes according to comments

--- src/main/java/uk/gov/hmcts/opal/filehandler/config/MarstonBaisFileBaisFileProcessorConfig.java
+++ src/main/java/uk/gov/hmcts/opal/filehandler/config/MarstonBaisFileBaisFileProcessorConfig.java
@@ -1,73 +1,10 @@
 package uk.gov.hmcts.opal.filehandler.config;
 
-import java.util.regex.Pattern;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
-import uk.gov.hmcts.opal.filehandler.entity.Interface;
-
 @Component("marstonBaisFileBaisFileProcessorConfig")
 @ConfigurationProperties(prefix = "opal.file-handler-service.bailiffs.marston")
-public class MarstonBaisFileBaisFileProcessorConfig  implements BaisFileProcessorConfiguration {
+public class MarstonBaisFileBaisFileProcessorConfig  extends AbstractBaisFileProcessorConfiguration {
 
-    private String containerName;
-    private String featureFlag;
-    private String fileNameRegex;
-    private String sftpUsername;
-    private Interface source;
-    private Interface target;
-
-    @Override
-    public String getContainerName() {
-        return containerName;
-    }
-
-    @Override
-    public String getFeatureFlag() {
-        return featureFlag;
-    }
-
-    @Override
-    public Pattern getFileNameRegex() {
-        return Pattern.compile(fileNameRegex);
-    }
-
-    @Override
-    public Interface getSource() {
-        return source;
-    }
-
-    @Override
-    public Interface getTarget() {
-        return target;
-    }
-
-    @Override
-    public String getSftpUsername() {
-        return sftpUsername;
-    }
-
-    public void setContainerName(String containerName) {
-        this.containerName = containerName;
-    }
-
-    public void setFeatureFlag(String featureFlag) {
-        this.featureFlag = featureFlag;
-    }
-
-    public void setFileNameRegex(String fileNameRegex) {
-        this.fileNameRegex = fileNameRegex;
-    }
-
-    public void setSftpUsername(String sftpUsername) {
-        this.sftpUsername = sftpUsername;
-    }
-
-    public void setSource(Interface source) {
-        this.source = source;
-    }
-
-    public void setTarget(Interface target) {
-        this.target = target;
-    }
 }
