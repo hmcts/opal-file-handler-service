f5cffd7a2fdd06d399c446c0834b767295d5ec07
Jordan Muscott <288038593+jordan-muscott-cgi@users.noreply.github.com>
2026-09-10 11:22:53 +0100

    remove unused import

--- src/main/java/uk/gov/hmcts/opal/filehandler/config/task/AutomatedCderFileTransferJob.java
+++ src/main/java/uk/gov/hmcts/opal/filehandler/config/task/AutomatedCderFileTransferJob.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.stereotype.Component;
-import uk.gov.hmcts.opal.filehandler.config.AllpayBaisFileProcessorConfiguration;
 import uk.gov.hmcts.opal.filehandler.config.CderBaisFileProcessorConfiguration;
 import uk.gov.hmcts.opal.filehandler.service.CderBaisFileProcessorService;
 
