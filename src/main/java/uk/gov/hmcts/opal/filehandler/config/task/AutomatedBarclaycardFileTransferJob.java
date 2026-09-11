0fc13f7fc738dcfad7321cadc792f0dfb571a855
Jordan Muscott <288038593+jordan-muscott-cgi@users.noreply.github.com>
2026-09-11 13:20:53 +0100

    removed unused imports

--- src/main/java/uk/gov/hmcts/opal/filehandler/config/task/AutomatedBarclaycardFileTransferJob.java
+++ src/main/java/uk/gov/hmcts/opal/filehandler/config/task/AutomatedBarclaycardFileTransferJob.java
@@ -1,12 +1,8 @@
 package uk.gov.hmcts.opal.filehandler.config.task;
 
-import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.opal.filehandler.config.BarclaycardBaisFileProcessorConfiguration;
 import uk.gov.hmcts.opal.filehandler.service.BarclaycardBaisFileProcessorService;
