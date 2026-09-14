714890c4e9869f9b9105a5754a9b0c88ada264cb
Sam Naylor <sam.naylor@hmcts.net>
2026-09-14 11:37:09 +0100

    mockito bean

--- src/integrationTest/java/uk/gov/hmcts/opal/filehandler/service/JacobsBaisFileProcessorServiceIntegrationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/opal/filehandler/service/JacobsBaisFileProcessorServiceIntegrationTest.java
@@ -12,7 +12,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.bean.override.mockito.MockitoSpyBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import uk.gov.hmcts.opal.common.launchdarkly.FeatureDisabledException;
 import uk.gov.hmcts.opal.common.launchdarkly.FeatureFlags;
 import uk.gov.hmcts.opal.filehandler.config.JacobsBaisFileProcessorConfiguration;
@@ -46,7 +46,7 @@ public class JacobsBaisFileProcessorServiceIntegrationTest
     @Autowired
     private BusinessUnitBankAccountEntityTestData buBankAccountTestData;
 
-    @MockitoSpyBean
+    @MockitoBean
     private FinesInterfaceFilePreprocessQueueService queue;
 
     @BeforeEach
