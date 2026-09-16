35c969417cb3201409b37d880f857db90b59a063
benedwards <ben.edwards@cgi.com>
2026-09-16 14:38:13 +0100

    Testing managed identity auth

--- src/main/java/uk/gov/hmcts/opal/filehandler/Application.java
+++ src/main/java/uk/gov/hmcts/opal/filehandler/Application.java
@@ -1,6 +1,12 @@
 package uk.gov.hmcts.opal.filehandler;
 
+import com.azure.identity.ManagedIdentityCredential;
+import com.azure.identity.ManagedIdentityCredentialBuilder;
+import com.azure.servicebus.jms.ServiceBusJmsConnectionFactory;
+import jakarta.annotation.PostConstruct;
+import jakarta.jms.ConnectionFactory;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
@@ -9,6 +15,7 @@ import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jms.core.JmsTemplate;
 import uk.gov.hmcts.opal.common.config.ServiceBusProperties;
 import uk.gov.hmcts.opal.filehandler.config.FeignConfiguration;
 import uk.gov.hmcts.opal.filehandler.util.TaskRunnerUtil;
@@ -30,5 +37,34 @@ public class Application {
         }
 
         SpringApplication.run(Application.class, args);
+    }
+
+    @PostConstruct
+    public void init() {
+        log.info("Application started");
+        JmsTemplate jmsTemplate = commonServiceBusJmsTemplate(commonServiceBusConnectionFactory());
+        jmsTemplate.convertAndSend("opal-common-servicebus-jms-template", "Test message");
+    }
+
+    private ConnectionFactory commonServiceBusConnectionFactory() {
+        ManagedIdentityCredential credential =
+            new ManagedIdentityCredentialBuilder()
+                .build();
+
+        String host = "opal-servicebus-stg.servicebus.windows.net";
+
+        return new ServiceBusJmsConnectionFactory(
+            credential,
+            host,
+            null
+        );
+    }
+
+    private JmsTemplate commonServiceBusJmsTemplate(ConnectionFactory connectionFactory) {
+        JmsTemplate jmsTemplate = new JmsTemplate(connectionFactory);
+        jmsTemplate.setDeliveryPersistent(true);
+        jmsTemplate.setExplicitQosEnabled(true);
+        jmsTemplate.setSessionTransacted(true);
+        return jmsTemplate;
     }
 }
