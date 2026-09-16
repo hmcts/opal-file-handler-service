9ca658ab7c91df45224aee3d787d09e794e19f5e
benedwards <ben.edwards@cgi.com>
2026-09-16 17:12:52 +0100

    Test

--- src/main/java/uk/gov/hmcts/opal/filehandler/Application.java
+++ src/main/java/uk/gov/hmcts/opal/filehandler/Application.java
@@ -1,10 +1,5 @@
 package uk.gov.hmcts.opal.filehandler;
 
-import com.azure.identity.ManagedIdentityCredential;
-import com.azure.identity.ManagedIdentityCredentialBuilder;
-import com.azure.servicebus.jms.ServiceBusJmsConnectionFactory;
-import jakarta.annotation.PostConstruct;
-import jakarta.jms.ConnectionFactory;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,7 +9,6 @@ import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.jms.core.JmsTemplate;
 import uk.gov.hmcts.opal.common.config.ServiceBusProperties;
 import uk.gov.hmcts.opal.filehandler.config.FeignConfiguration;
 import uk.gov.hmcts.opal.filehandler.util.TaskRunnerUtil;
@@ -38,36 +32,36 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
-//    @PostConstruct
-//    public void init() {
-//        try {
-//            log.info("Application started");
-//            JmsTemplate jmsTemplate = commonServiceBusJmsTemplate(commonServiceBusConnectionFactory());
-//            jmsTemplate.convertAndSend("opal-common-servicebus-jms-template", "Test message");
-//        } catch (Throwable e) {
-//            log.info(e.getMessage());
-//        }
-//    }
-//
-//    private ConnectionFactory commonServiceBusConnectionFactory() {
-//        ManagedIdentityCredential credential =
-//            new ManagedIdentityCredentialBuilder()
-//                .build();
-//
-//        String host = "opal-servicebus-stg.servicebus.windows.net";
-//
-//        return new ServiceBusJmsConnectionFactory(
-//            credential,
-//            host,
-//            null
-//        );
-//    }
-//
-//    private JmsTemplate commonServiceBusJmsTemplate(ConnectionFactory connectionFactory) {
-//        JmsTemplate jmsTemplate = new JmsTemplate(connectionFactory);
-//        jmsTemplate.setDeliveryPersistent(true);
-//        jmsTemplate.setExplicitQosEnabled(true);
-//        jmsTemplate.setSessionTransacted(true);
-//        return jmsTemplate;
-//    }
+    //    @PostConstruct
+    //    public void init() {
+    //        try {
+    //            log.info("Application started");
+    //            JmsTemplate jmsTemplate = commonServiceBusJmsTemplate(commonServiceBusConnectionFactory());
+    //            jmsTemplate.convertAndSend("opal-common-servicebus-jms-template", "Test message");
+    //        } catch (Throwable e) {
+    //            log.info(e.getMessage());
+    //        }
+    //    }
+    //
+    //    private ConnectionFactory commonServiceBusConnectionFactory() {
+    //        ManagedIdentityCredential credential =
+    //            new ManagedIdentityCredentialBuilder()
+    //                .build();
+    //
+    //        String host = "opal-servicebus-stg.servicebus.windows.net";
+    //
+    //        return new ServiceBusJmsConnectionFactory(
+    //            credential,
+    //            host,
+    //            null
+    //        );
+    //    }
+    //
+    //    private JmsTemplate commonServiceBusJmsTemplate(ConnectionFactory connectionFactory) {
+    //        JmsTemplate jmsTemplate = new JmsTemplate(connectionFactory);
+    //        jmsTemplate.setDeliveryPersistent(true);
+    //        jmsTemplate.setExplicitQosEnabled(true);
+    //        jmsTemplate.setSessionTransacted(true);
+    //        return jmsTemplate;
+    //    }
 }
