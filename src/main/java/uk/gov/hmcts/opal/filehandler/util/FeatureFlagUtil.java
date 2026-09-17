25d269d63e562b2c8e8bc496bd6cfad381239aca
benedwards <ben.edwards@cgi.com>
2026-09-17 17:30:47 +0100

    Test

--- src/main/java/uk/gov/hmcts/opal/filehandler/util/FeatureFlagUtil.java
+++ src/main/java/uk/gov/hmcts/opal/filehandler/util/FeatureFlagUtil.java
@@ -1,19 +1,25 @@
 package uk.gov.hmcts.opal.filehandler.util;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.opal.common.launchdarkly.FeatureDisabledException;
+import uk.gov.hmcts.opal.common.launchdarkly.config.LaunchDarklyProperties;
 import uk.gov.hmcts.opal.common.launchdarkly.service.FeatureToggleApi;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class FeatureFlagUtil {
 
     private final FeatureToggleApi featureToggleApi;
+    private final LaunchDarklyProperties properties;
 
     public void requireEnabledFeature(String feature) {
+        log.info("TMP: properties.isEnabled() '{}'", properties.isEnabled());
+        log.info("TMP: properties.getEnv() '{}'", properties.getEnv());
         if (!featureToggleApi.isFeatureEnabled(feature)) {
-            throw new FeatureDisabledException(feature + " is not enabled");
+            throw new FeatureDisabledException("'" + feature + "' is not enabled");
         }
     }
 
