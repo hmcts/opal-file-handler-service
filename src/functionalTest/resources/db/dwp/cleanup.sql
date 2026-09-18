94e05435eb76e6c5a8080139d7d313a2483a4f2e
Calvin Chu <calvin.chu@hmcts.net>
2026-09-18 12:43:32 +0100

    Initial commit for DWP transfer test and and steps

---
+++ src/functionalTest/resources/db/dwp/cleanup.sql
@@ -0,0 +1,5 @@
+DELETE FROM public.interface_files
+WHERE source = 'DWP' AND file_name = '0000015232_dat_0000000612_08011008_111355.txt';
+
+DELETE FROM public.business_unit_bank_account
+WHERE business_unit_bank_account_id = 9000000000000020;
