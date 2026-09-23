7bd4fa01e86f8f16d81d17022d4f1af593121ee8
Conroy White <Conroy.White@HMCTS.NET>
2026-09-23 18:45:39 +0100

    fixed checkstyle issues

--- src/main/java/uk/gov/hmcts/opal/filehandler/config/FileProcessorConfiguration.java
+++ src/main/java/uk/gov/hmcts/opal/filehandler/config/FileProcessorConfiguration.java
@@ -5,6 +5,9 @@ import uk.gov.hmcts.opal.filehandler.entity.Interface;
 public interface FileProcessorConfiguration {
 
     String getFeatureFlag();
+
     Interface getSource();
+
     Interface getTarget();
+
 }
