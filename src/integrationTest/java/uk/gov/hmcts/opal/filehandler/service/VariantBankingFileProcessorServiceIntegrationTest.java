05fec8e9fc36b28e5039381c0d67996c325332a3
Conroy White <Conroy.White@HMCTS.NET>
2026-09-23 17:53:02 +0100

    Write test to fulfill po8744 variant banking file upload

---
+++ src/integrationTest/java/uk/gov/hmcts/opal/filehandler/service/VariantBankingFileProcessorServiceIntegrationTest.java
@@ -0,0 +1,109 @@
+package uk.gov.hmcts.opal.filehandler.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+import uk.gov.hmcts.opal.common.launchdarkly.FeatureDisabledException;
+import uk.gov.hmcts.opal.common.launchdarkly.FeatureFlags;
+import uk.gov.hmcts.opal.filehandler.config.VariantBankingFileProcessorConfig;
+import uk.gov.hmcts.opal.filehandler.support.AbstractBaisFileProcessorServiceIntegrationTest;
+
+@ActiveProfiles("integration")
+@TestPropertySource(properties = {
+    "launchdarkly.default-flag-values.variant-banking=true"
+})
+@DisplayName("Variant Banking File Processor Service Integration Tests")
+class VariantBankingFileProcessorServiceIntegrationTest
+    extends AbstractBaisFileProcessorServiceIntegrationTest {
+
+    @Autowired
+    private VariantBankingFileProcessorService service;
+
+    @Autowired
+    private VariantBankingFileProcessorConfig config;
+
+    @BeforeEach
+    void setUp() {
+        repository.deleteAll();
+        blobServiceClient.createBlobContainerIfNotExists(config.getContainerName());
+    }
+
+    @Nested
+    @TestPropertySource(properties = {
+        "launchdarkly.default-flag-values.release-1c-banking-interfaces=false",
+        "launchdarkly.default-flag-values.variant-banking=true"
+    })
+    class BankingInterfacesDisabled {
+
+        @Test
+        @DisplayName("AC1: Feature flag 'release-1c-banking-interfaces' is false")
+        void bankingInterfacesIsDisabled() {
+
+            FeatureDisabledException exception =
+                assertThrows(
+                    FeatureDisabledException.class,
+                    () -> service.run(config)
+                );
+
+            assertThat(exception)
+                .hasMessage(
+                    FeatureFlags.RELEASE_1C_BANKING_INTERFACES
+                        + " is not enabled"
+                );
+        }
+    }
+
+    @Nested
+    @TestPropertySource(properties = {
+        "launchdarkly.default-flag-values.release-1c-banking-interfaces=true",
+        "launchdarkly.default-flag-values.variant-banking=false"
+    })
+    class VariantBankingDisabled {
+
+        @Test
+        @DisplayName("AC1: Feature flag 'variant-banking' is false")
+        void variantBankingIsDisabled() {
+
+            FeatureDisabledException exception =
+                assertThrows(
+                    FeatureDisabledException.class,
+                    () -> service.run(config)
+                );
+
+            assertThat(exception)
+                .hasMessage("variant-banking is not enabled");
+        }
+    }
+
+    @Nested
+    @TestPropertySource(properties = {
+        "launchdarkly.default-flag-values.release-1c-banking-interfaces=false",
+        "launchdarkly.default-flag-values.variant-banking=false"
+    })
+    class BothFeatureFlagsDisabled {
+
+        @Test
+        @DisplayName("AC1: Both feature flags are false")
+        void bothFeatureFlagsAreDisabled() {
+
+            FeatureDisabledException exception =
+                assertThrows(
+                    FeatureDisabledException.class,
+                    () -> service.run(config)
+                );
+
+            assertThat(exception)
+                .hasMessage(
+                    FeatureFlags.RELEASE_1C_BANKING_INTERFACES
+                        + " is not enabled"
+                );
+        }
+    }
+}
---
+++ src/main/java/uk/gov/hmcts/opal/filehandler/config/FileProcessorConfiguration.java
@@ -0,0 +1,10 @@
+package uk.gov.hmcts.opal.filehandler.config;
+
+import uk.gov.hmcts.opal.filehandler.entity.Interface;
+
+public interface FileProcessorConfiguration {
+
+    String getFeatureFlag();
+    Interface getSource();
+    Interface getTarget();
+}
